e681576aa787ff22dfbdda7c89784eea733cfe12
Davide <davidakinola26@gmail.com>
2025-02-04 11:15:57 +0100

    Add API to return mathematical properties of a number

--- index.js
+++ index.js
@@ -1,82 +1,79 @@
-// Importing the express library
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// Function to get mathematical properties of a number
-function getMathProperties(num) {
-    const isPrime = (n) => {
-        if (n <= 1) return false;
-        for (let i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i === 0) return false;
-        }
-        return true;
-    };
-
-    const isPerfect = (n) => {
-        let sum = 0;
-        for (let i = 1; i < n; i++) {
-            if (n % i === 0) sum += i;
-        }
-        return sum === n;
-    };
-
-    const isArmstrong = (n) => {
-        const digits = n.toString().split('');
-        const sum = digits.reduce((acc, digit) => acc + Math.pow(Number(digit), digits.length), 0);
-        return sum === n;
-    };
-
-    const digitSum = num
-        .toString()
-        .split('')
-        .map(Number)
-        .reduce((acc, val) => acc + val, 0);
-
-    const properties = [];
-    if (isArmstrong(num)) properties.push('armstrong');
-    if (num % 2 === 0) properties.push('even');
-    else properties.push('odd');
-    if (isPerfect(num)) properties.push('perfect');
-
-    const funFact = isArmstrong(num)
-        ? `${num} is an Armstrong number because ${num
-              .toString()
-              .split('')
-              .map((digit) => `${digit}^${num.toString().length}`)
-              .join(' + ')} = ${num}`
-        : `${num} is not an Armstrong number.`;
-
-    return {
-        number: num,
-        is_prime: isPrime(num),
-        is_perfect: isPerfect(num),
-        properties: properties,
-        digit_sum: digitSum,
-        fun_fact: funFact
-    };
+// Function to check if the number is prime
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
 }
 
-// Define the GET route
-app.get('/number/:num', (req, res) => {
-    const num = req.params.num;
+// Function to check if the number is a perfect number
+function isPerfect(num) {
+    let sum = 0;
+    for (let i = 1; i < num; i++) {
+        if (num % i === 0) sum += i;
+    }
+    return sum === num;
+}
 
-    // Check if the input is a valid integer
-    const numAsInt = parseInt(num, 10);
+// Function to check if the number is an Armstrong number
+function isArmstrong(num) {
+    let sum = 0;
+    let digits = num.toString().split('');
+    let numDigits = digits.length;
 
-    if (isNaN(numAsInt)) {
-        // If input is invalid, return 400 Bad Request with the new format
+    for (let digit of digits) {
+        sum += Math.pow(parseInt(digit), numDigits);
+    }
+
+    return sum === num;
+}
+
+// Function to calculate the sum of digits
+function digitSum(num) {
+    return num.toString().split('').reduce((sum, digit) => sum + parseInt(digit), 0);
+}
+
+// Fun fact generation based on Armstrong number check
+function getFunFact(num) {
+    if (isArmstrong(num)) {
+        const digits = num.toString().split('');
+        const sumOfPowers = digits.map(digit => Math.pow(parseInt(digit), digits.length)).join(' + ');
+        return `${num} is an Armstrong number because ${sumOfPowers} = ${num}`;
+    }
+    return '';
+}
+
+// Main API Route
+app.get('/api/number/:num', (req, res) => {
+    const num = parseInt(req.params.num);
+
+    if (isNaN(num)) {
         return res.status(400).json({
-            number: num,
+            number: req.params.num,
             error: true
         });
     }
 
-    const result = getMathProperties(numAsInt);
-    return res.json(result);
+    const result = {
+        number: num,
+        is_prime: isPrime(num),
+        is_perfect: isPerfect(num),
+        properties: [],
+        digit_sum: digitSum(num),
+        fun_fact: getFunFact(num)
+    };
+
+    if (isArmstrong(num)) result.properties.push("armstrong");
+    if (num % 2 !== 0) result.properties.push("odd");
+
+    res.status(200).json(result);
 });
 
-// Start the server
 app.listen(port, () => {
-    console.log(`Math API listening at http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
